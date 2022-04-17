529c8351751da0de1eab7338e568dda55757fcd3
LachieDaly <Lachiedaly@gmail.com>
2022-04-17 21:15:33 +1000

    completely reworked website

--- README.md
+++
@@ -1,5 +0,0 @@
-### Hotel Website
-Website from the beginning of university
-
-## Usage
-Download files, open path to index.html in favourite browser
--- js/script.js
+++ js/script.js
@@ -1,18 +1,59 @@
-var window_width = $(window).width() - $('#object').width();
-console.log(window_width);
+/**
+ * script.js
+ * 
+ * Author: Lachlan Daly
+ * Date: 09-Jun-2021
+ */
 
-var document_height = $(document).height() - $(window).height();
-console.log(document_height);
-document_height = 3000;
+ document.addEventListener("readystatechange", function(event) {
+	if(event.target.readyState == "interactive") {
 
-$(function () {
-    $(window).scroll(function () {
-        var scroll_position = $(window).scrollTop();
-        console.log("scrolling")
-        var object_position_left = window_width * (scroll_position / document_height) + 500;
-        console.log(object_position_left)
-        $('#object').css({
-            'left': object_position_left
-        });
-    });
+		// Adding a "JavaScript is Enabled" Body Class
+
+		document.querySelector("body").classList.add("js");
+
+        // This is a Javascript Plugin beware!
+        // This is not my original code
+		AOS.init();
+
+        // This is for tiny-slider - A JavaScript Plugin
+        // This is not my original code
+        // Reading up on the documentation, I found that
+        // many sliders can be made
+        var slider1 = tns({
+			container: ".slider1",
+			controlsPosition: "bottom",
+			nav: false,
+			autoplayButtonOutput: false,
+			items: 1,
+			slideBy: "page",
+			autoplay: true
+		});
+
+        var slider2 = tns({
+			container: ".slider2",
+			controlsPosition: "bottom",
+			nav: false,
+			autoplayButtonOutput: false,
+			items: 1,
+			slideBy: "page",
+			autoplay: true
+		});
+
+        var slider3 = tns({
+			container: ".slider3",
+			controlsPosition: "bottom",
+			nav: false,
+			autoplayButtonOutput: false,
+			items: 1,
+			slideBy: "page",
+			autoplay: true
+		});
+
+        function validateForm() {
+            window.location.replace("thank-you.html")
+        }
+
+
+    }
 });
